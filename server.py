ccde3683e760fabddd708459afc4b12e07580d64
xinyu.kou@macbook <kouyu66@163.com>
2019-03-13 22:27:57 +0800

    优化Inbox类中的数据接收方法，支持大批量数据间断传输。未测试

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import struct
 import threading
 import time
 import json
@@ -43,13 +44,26 @@ class Inbox():
             def dataRecv(self, sock, addr, lock):
                 '''获取锁以后，每1024个字节收一次数据包，解析并添加到类属性列表中，同时设置led状态为True，通讯完毕后端口关闭，释放锁'''
                 lock.acquire()
+                dataBuffer = bytes()
+                headerSize = 4
                 while True:    
                     data = sock.recv(1024)
-                    if not data:
+                    if data:
+                        dataBuffer += data
+                        if len(dataBuffer) < headerSize:
+                            continue
+                        headpack = struct.unpack('!I',dataBuffer[:headerSize])
+                        bodySize = headpack[0]
+                        if len(dataBuffer) < headerSize + bodySize:
+                            continue
+                        body = dataBuffer[headerSize:headerSize+bodySize]
+                        decode_data = json.loads(body.decode('utf-8'))
+                        self.__record.append(decode_data)
+                        self.__led = True
+                        dataBuffer = bytes()
                         break
-                    decode_data = json.loads(data.decode('utf-8'))
-                    self.__record.append(decode_data)
-                    self.__led = True
+                    else:
+                        break
                 lock.release()
                 sock.close()
                 return
@@ -106,20 +120,20 @@ def load_main_dict(main_dict_file):
 
     if os.path.exists(main_dict_file):
         with open(main_dict_file, 'r') as main_dict_obj:
-            main_dict = json.loads(main_dict_obj)
+            main_dict = json.load(main_dict_obj)
     else:
-        main_dict = {}
+        main_dict = dict()
     return main_dict
 
 def process(message):
-    if isinstance(message, dict) and 'type' in message:
-        message_type = message.pop('type')
+    if isinstance(message, dict) and "type" in message:
+        message_type = message.pop("type")
     else:
         raise ValueError("message type error, it should be dict and contain 'type' key")
     
-    if message_type == 'monitor':
+    if message_type == "monitor":
         monitor_processor(message)
-    elif message_type == 'powercycle':
+    elif message_type == "powercycle":
         powercycle_processor(message)
     
 def powercycle_processor(message):
@@ -128,34 +142,36 @@ def powercycle_processor(message):
 
 def monitor_processor(message):
     '''主字典刷新及log记录操作'''
-    identifier = message.pop('id') # id should be timestamp + ip string,
-    info_level = message.pop('level') # 0 init 1 update
+    identifier = message.pop("id") # id should be timestamp + ip string,
+    info_level = message.pop("level") # 0 init 1 update 2 heart_beat
     if info_level is 0:
         sub_init_process(identifier, message)
     elif info_level is 1:
-        server_info = message.get('server_info')
-        script_info = message.get('script_info')
-        ssd_info = message.get('ssd_info')
+        server_info = message.get("server_info")
+        script_info = message.get("script_info")
+        ssd_info = message.get("ssd_info")
         if server_info:
-            sub_single_dict_process('server_info',server_info,identifier)
+            sub_single_dict_process("server_info",server_info,identifier)
         if script_info:
-            sub_single_dict_process('script_info',script_info,identifier)
+            sub_single_dict_process("script_info",script_info,identifier)
         if ssd_info:
             sub_ssd_process(ssd_info, identifier)
-
+    elif info_level is 2:
+        info = '[{0}] ^^^ Alive Detected ^^^'.format(identifier)
+        main_log.write(info)
+    main_dict.update({identifier : {"server_info":server_info,"script_info":script_info,"ssd_info":ssd_info}})
     return
 
 def sub_init_process(identifier, message):
-    server_info = message.get('server_info')
-    ssd_info = message.get('ssd_info')
+    server_info = message.get("server_info")
+    ssd_info = message.get("ssd_info")
     info = '====== New Clietn Join ======\n'
-    for key, value in server_info:
+    for key, value in server_info.items():
         info = info + '{0} : {1}\n'.format(key, value)
-    for key, value in ssd_info:
+    for key, value in ssd_info.items():
         info = info + '{0} : {1}\n'.format(key, value)
     main_log.write(info)
     warning_log.write(info)
-    main_dict.update({identifier : message})
 
 def sub_single_dict_process(single_dict_name, single_dict_info, identifier):
     old_record = main_dict.get(identifier)
@@ -178,17 +194,17 @@ def sub_single_dict_process(single_dict_name, single_dict_info, identifier):
                 info = '[{0}] ??? Change Detected ??? [change {1} : {2} ===> {3}\n]'.format(identifier,key,old_dict_info[key],single_dict_info[key])
     else:
         print('{0} info not found, init client to resend the message.'.format(identifier))
-    main_dict.update({identifier : {single_dict_name : single_dict_info}})
+
     return
 
 def sub_ssd_process(new_ssd_info, identifier):
-    old_ssd_info = main_dict[identifier]['ssd_info']
+    old_ssd_info = main_dict[identifier]["ssd_info"]
     increase_ssd_bus, decrease_ssd_bus, diff_ssd_bus = dict_comp.compare(new_ssd_info, old_ssd_info)
 
     if increase_ssd_bus:
         for bus_num in increase_ssd_bus:
             info = '[{0}] +++ SSD Insert Detected +++\n'.format(identifier)
-            for key, value in new_ssd_info[bus_num]:
+            for key, value in new_ssd_info[bus_num].items():
                 info = info + '[{0} : {1}]'.format(key, value)
             info = info + '+++                     +++\n'
             main_log.write(info)
@@ -217,7 +233,6 @@ def sub_ssd_process(new_ssd_info, identifier):
         main_log.write(info)
         warning_log.write(info)
 
-
 # ------ Danger Zone ------ #
 main_dict_file = 'main_dict.json'
 monitor_inbox = Inbox(1025) # 建立监控服务器
@@ -234,4 +249,7 @@ while True:
         message = monitor_inbox.vomit()
         process(message)
     else:
-        time.sleep(0.5)
+        time.sleep(1)
+
+    with open(main_dict_file, 'w') as db:
+        json.dump(main_dict, db)
